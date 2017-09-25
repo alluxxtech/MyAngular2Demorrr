a49de844bb7d1705cb3af767e493b9892388625e
Sergiy <slevin.uk@gmail.com>
2017-09-25 19:20:21 +0300

    Update with dog

--- Angular2Demo/src/app/app.component.js
+++ Angular2Demo/src/app/app.component.js
@@ -5,11 +5,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.pageHeader = 'Employee Details';
+        this.pageHeader = null /*'Employee Details'*/;
+        this.imagePath = '/image/dog.jpg';
+        this.firstName = 'Peter';
+        this.lastName = 'Pen';
     }
+    AppComponent.prototype.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -5,5 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html'
 })
 export class AppComponent  {
-    pageHeader = 'Employee Details';
+    pageHeader: string = null /*'Employee Details'*/;
+    imagePath: string = '/image/dog.jpg';
+    firstName: string = 'Peter';
+    lastName: string = 'Pen';
+    getFullName(): string {
+    return this.firstName + " " + this.lastName;
+}
+
 }
